56696c5f7ddec9747a6246508426b78103c65389
Memo0o2013 <mohamedabdelaal2013@gmail.com>
2020-08-12 23:30:35 +0200

    final Touches

--- src/app/components/contactus/contactus.component.ts
+++ src/app/components/contactus/contactus.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-contactus',
@@ -11,5 +13,13 @@ export class ContactusComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  reg = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{3,4}$";
+  regForm = new FormGroup({
+    'name': new FormControl('', [Validators.required , Validators.minLength(6) , Validators.maxLength(18)]),
+    'email':new FormControl('' , [Validators.required, Validators.pattern(this.reg)]),
+    'subject': new FormControl('', [Validators.required, Validators.max(60), Validators.min(4)]),
+    'message':new FormControl('', [Validators.required, Validators.max(300), Validators.min(10)])
+  })
 
 }
